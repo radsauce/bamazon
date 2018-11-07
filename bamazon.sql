0aa07bdbdcf0ee4ec704c0213ca3d0ea6c614bff
Radsauce <radley.eakle@yahoo.com>
2018-11-06 17:57:48 -0800

    push to turn in

--- bamazon.sql
+++ bamazon.sql
@@ -7,10 +7,12 @@ DROP TABLE products;
 
 USE bamazon_db;
 CREATE TABLE products (
+    item_id INT AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(100) NOT NULL,
     department_name VARCHAR(100) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     stock_quantity INT NOT NULL,
+    PRIMARY KEY (item_id)
 );
 
 USE bamazon_db;
--- models/bamazon.js
+++ models/bamazon.js
@@ -1,5 +1,10 @@
 module.exports = (sequelize, DataTypes) => 
   sequelize.define('products', {
+    item_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     product_name: DataTypes.STRING,
     department_name: DataTypes.STRING,
     price: DataTypes.DECIMAL,
